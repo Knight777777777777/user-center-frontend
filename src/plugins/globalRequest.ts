591eec6c90f41cfed29e0860aff380214fc814bc
WaterBird <lcc1105897090@126.com>
2023-10-09 21:03:42 +0800

    add Dockerfile

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -9,6 +9,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
+  prefix: process.env.NODE_ENV === 'production' ? 'http://user-center-frontend.cn' : undefined
   // requestType: 'form',
 });
 
